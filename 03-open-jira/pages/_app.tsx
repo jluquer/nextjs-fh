7c1380e18ea7cb0cb5c02d5b3e3ec4d640f1466d
Javier Luque <javier.lr9@gmail.com>
2023-10-11 23:07:59 +0200

    base project with mui

---
+++ 03-open-jira/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const basicTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
