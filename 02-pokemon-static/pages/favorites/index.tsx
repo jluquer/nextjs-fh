0c52a6a694d8cf3ff1ae46f38c7ca9be28f6c72c
Javier Luque <javier.lr9@gmail.com>
2023-10-11 10:18:37 +0200

    favorites

--- 02-pokemon-static/pages/favorites/index.tsx
+++ 02-pokemon-static/pages/favorites/index.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 
-import { Layout } from '../../components/layouts';
+import { Layout } from "../../components/layouts";
+import { NoFavorites } from "../../components/ui";
+import { localFavorites } from "../../utils";
+import { FavoritePokemons } from "../../components/pokemon";
 
 const FavoritesPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
   return (
-      <Layout title='Pokémons - Favoritos'>
-        <h1>Favoritos</h1>
-      </Layout>
-  )
+    <Layout title="Pokémons - Favoritos">
+      {!favoritePokemons.length ? (
+        <NoFavorites />
+      ) : (
+        <FavoritePokemons favoritePokemons={favoritePokemons} />
+      )}
+    </Layout>
+  );
 };
 
 export default FavoritesPage;
