aca1fb2cdfb6f94078ef4f2aae5d29e1fbd068fd
Javier Luque <javier.lr9@gmail.com>
2023-10-25 14:32:21 +0200

    initialized directories and ShopLayout

--- 05-teslo-shop/pages/index.tsx
+++ 05-teslo-shop/pages/index.tsx
@@ -1,3 +1,18 @@
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
+
 export default function Home() {
-  return <h1>Teslo Shop</h1>;
+  return (
+    <ShopLayout
+      title="Teslo-Shop - Home"
+      pageDescription="Encuentra los mejores productos de Teslo aquí"
+    >
+      <Typography variant="h1" component={"h1"}>
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  );
 }
