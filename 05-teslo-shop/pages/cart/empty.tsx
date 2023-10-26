9b166fd752e6c5669003d92f6562e89950cbe00f
Javier Luque <javier.lr9@gmail.com>
2023-10-26 10:28:06 +0200

    empty cart

---
+++ 05-teslo-shop/pages/cart/empty.tsx
@@ -0,0 +1,36 @@
+import NextLink from 'next/link';
+
+import { Box, Link, Typography } from '@mui/material';
+import { RemoveShoppingCartOutlined } from '@mui/icons-material';
+
+import { ShopLayout } from '@/components/layouts';
+
+function EmptyPage() {
+  return (
+    <ShopLayout title="Carrito vació" pageDescription="No hay artículos en el carrito de compras">
+      <Box
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'calc(100vh - 200px)'}
+        sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+      >
+        <RemoveShoppingCartOutlined
+          sx={{
+            fontSize: 100,
+          }}
+        />
+        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'}>
+          <Typography>Su carrito esta vació</Typography>
+          <NextLink href={'/'} passHref legacyBehavior>
+            <Link typography={'h4'} color={'secondary'}>
+              Regresar
+            </Link>
+          </NextLink>
+        </Box>
+      </Box>
+    </ShopLayout>
+  );
+}
+
+export default EmptyPage;
