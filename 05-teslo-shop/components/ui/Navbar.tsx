19126ec76e42a70d9d7536351f04624e323da94a
Javier Luque <javier.lr9@gmail.com>
2023-10-27 13:11:03 +0200

    navbar active link category

--- 05-teslo-shop/components/ui/Navbar.tsx
+++ 05-teslo-shop/components/ui/Navbar.tsx
@@ -1,13 +1,22 @@
-import NextLink from "next/link";
-import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from "@mui/material";
-import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+import NextLink from 'next/link';
+import { usePathname } from 'next/navigation';
 
+import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
+import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+
+const categories = [
+  { name: 'Hombres', url: '/category/men' },
+  { name: 'Mujeres', url: '/category/women' },
+  { name: 'Niños', url: '/category/kid' },
+];
 export function Navbar() {
+  const path = usePathname();
+
   return (
     <AppBar>
       <Toolbar>
-        <NextLink href={"/"} passHref legacyBehavior={true}>
-          <Link display={"flex"} alignItems={"center"}>
+        <NextLink href={'/'} passHref legacyBehavior={true}>
+          <Link display={'flex'} alignItems={'center'}>
             <Typography variant="h6">Teslo |</Typography>
             <Typography sx={{ ml: 0.5 }}>Shop</Typography>
           </Link>
@@ -15,22 +24,14 @@ export function Navbar() {
 
         <Box flex={1}></Box>
 
-        <Box sx={{ display: { xs: "none", sm: "block" } }}>
-          <NextLink href={"/category/men"} passHref legacyBehavior={true}>
-            <Link>
-              <Button>Hombres</Button>
-            </Link>
-          </NextLink>
-          <NextLink href={"/category/women"} passHref legacyBehavior={true}>
-            <Link>
-              <Button>Mujeres</Button>
-            </Link>
-          </NextLink>
-          <NextLink href={"/category/kid"} passHref legacyBehavior={true}>
-            <Link>
-              <Button>Niños</Button>
-            </Link>
-          </NextLink>
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          {categories.map((category) => (
+            <NextLink key={category.url} href={category.url} passHref legacyBehavior={true}>
+              <Link>
+                <Button color={path === category.url ? 'primary' : 'info'}>{category.name}</Button>
+              </Link>
+            </NextLink>
+          ))}
         </Box>
 
         <Box flex={1}></Box>
@@ -38,7 +39,7 @@ export function Navbar() {
         <IconButton>
           <SearchOutlined />
         </IconButton>
-        <NextLink href={"/cart"} passHref legacyBehavior={true}>
+        <NextLink href={'/cart'} passHref legacyBehavior={true}>
           <Link>
             <IconButton>
               <Badge badgeContent={2} color="secondary">
