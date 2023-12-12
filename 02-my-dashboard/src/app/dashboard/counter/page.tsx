35af0f237f78c019226c370f7b5b2e04a342d39f
Javier Luque <javier.lr9@gmail.com>
2023-12-12 23:39:06 +0100

    fin sección 4

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,15 @@
+import { CartCounter } from '@/app/shopping-cart';
+
+export const metadata = {
+  title: 'Counter Page',
+  description: 'Counter Page',
+};
 
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Hello Page counter</h1>
+    <div className='flex flex-col items-center justify-center w-full h-full'>
+      <span>Productos en el carrito</span>
+      <CartCounter value={100} />
     </div>
   );
 }
---
+++ 02-my-dashboard/src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useState } from 'react';
+
+interface Props {
+  value?: number;
+}
+
+export function CartCounter({ value = 0 }: Props) {
+  const [count, setCount] = useState(value);
+
+  return (
+    <>
+      <span className='text-9xl'>{count}</span>
+      <div className='flex gap-2'>
+        <button
+          className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px]'
+          onClick={() => setCount(count - 1)}
+        >
+          -1
+        </button>
+        <button
+          className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px]'
+          onClick={() => setCount(count + 1)}
+        >
+          +1
+        </button>
+      </div>
+    </>
+  );
+}
