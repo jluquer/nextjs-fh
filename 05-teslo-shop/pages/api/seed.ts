7f6b3e569304de9b5a2853c9427e371b10aa23b7
Javier Luque <javier.lr9@gmail.com>
2023-12-12 09:00:48 +0100

    course updated to app router

--- 05-teslo-shop/pages/api/seed.ts
+++ 05-teslo-shop/pages/api/seed.ts
@@ -1,12 +1,15 @@
 import { db, seedDatabase } from '@/database';
-import { Product } from '@/models';
+import { Product, UserModel } from '@/models';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   message: string;
 };
 
-export default async function seed(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function seed(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
   if (process.env.NODE_ENV === 'production')
     return res.status(401).json({ message: "Can't seed in production" });
 
@@ -14,6 +17,9 @@ export default async function seed(req: NextApiRequest, res: NextApiResponse<Dat
 
   await Product.deleteMany();
   await Product.insertMany(seedDatabase.initialData.products);
+  
+  await UserModel.deleteMany();
+  await UserModel.insertMany(seedDatabase.initialData.users);
 
   await db.disconnect();
 
